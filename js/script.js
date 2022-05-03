6d3e95c52b376922b9c99954f9e0a1766bd2e6f3
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-05-03 15:05:57 +0000

    EDIT

--- js/script.js
+++ js/script.js
@@ -7,17 +7,22 @@
 function displayNumber () {
   // initialize variables and constant
 	let answer = ""
-  
+  const MIN_R = 18
+  const MIN_PG = 13
 // get user input
 	let age = parseInt(document.getElementById('age').value)
 
   	// if guess is right says correct
-	if (age >= 18) {
-		answer = "You can see an R-rated movie"
+	if (age >= MIN_R) {
+		answer = "You can see an R-rated movie!"
 	} 
-	//if not says incorrect	
-	else if {
-		answer = "Cold. Bring a sweater."
+	//if else 	
+	else if (age >= MIN_PG) {
+		answer = "You can see a PG rated movie!"
+	}
+
+  else  {
+		answer = "You can see a G-rated movie!"
 	}
 
   // display the results
